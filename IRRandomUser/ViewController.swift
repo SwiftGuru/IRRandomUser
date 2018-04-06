ea3b7989bf72b86b2197fc1c9c26e89bdc29b3ee
Ihor <ihor@rudych.com>
2018-04-06 15:54:10 -0600

    Initial Commit

---
+++ IRRandomUser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IRRandomUser
+//
+//  Created by Ihor Rudych on 4/6/18.
+//  Copyright © 2018 Ihor Rudych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
